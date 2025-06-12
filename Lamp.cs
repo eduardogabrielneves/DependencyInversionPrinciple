dfb12d5e8b7db6c18bb658f54b127ee8b7023d5b
Eduardo Gabriel Neves <144508939+eduardogabrielneves@users.noreply.github.com>
2025-06-12 20:34:26 -0300

    antigo

---
+++ Lamp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    enum State { On, Off }
+    class Lamp(State State)
+    {
+        public void Operate()
+        {
+            State = State == State.On ? State.Off : State.On;
+            Console.WriteLine("Luz " + (State == State.On ? "Ligada" : "Desligada"));
+        }
+        class Switch
+        {
+            private Lamp lamp;
+            public Switch(Lamp device)
+            {
+                this.lamp = device;
+            }
+            public void Press()
+            {
+                lamp.Operate();
+            }
+        }
+    }
+}
